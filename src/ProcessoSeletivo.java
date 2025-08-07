a65616485a9bee307f55a7148bd80c02ba8f2cdd
thiagoferrer <thi-ferrer@live.com>
2025-08-07 10:38:03 -0300

    fazendo ligações para cada candidato selecionado

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,10 +1,38 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+
     }
 
+    //Ligações para candidatos selecionados
+    public static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa.");
+        else
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo tentativas " + tentativasRealizadas + " realizada");
+    }
+
+    public static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    //Lista de candidatos selecionados
     public static void imprimirSelecionados() {
         String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
@@ -18,6 +46,7 @@ public class ProcessoSeletivo {
             System.out.println("O candidato selecionado foi " + candidato);
     }
 
+    //Selecionando candidatos
     public static void selecaoCandidatos() {
         String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
 
@@ -42,6 +71,7 @@ public class ProcessoSeletivo {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
+    //Analisando candidatos
     public static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
